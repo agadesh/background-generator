813c34b355fc340d5f5961f0755d63bddaa6c8b6
Adesh Agarwal <adeshagarwal440@gmail.com>
2020-07-25 20:07:57 +0530

    added copy feature, css changes

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("grad");
+var col1 = document.querySelector("#col1");
+var col2 = document.querySelector("#col2");
+var text = document.querySelector("#text");
+var body = document.querySelector("body");
+var alertdone = document.querySelector(".alertdone");
 
+col1.addEventListener("input", setGradient);
+
+col2.addEventListener("input", setGradient);
 function setGradient() {
-  body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
-
-  css.textContent = body.style.background + ";";
+  alertdone.classList.add("done");
+  body.style.background = "linear-gradient(to right," + col1.value + "," + col2.value + ")";
+  text.value = body.style.background;
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+// w3
+function myFunction() {
+  /* Get the text field */
+  text.toggleAttribute("disabled");
+  var copyText = text;
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+  text.toggleAttribute("disabled");
+  /* Alert the copied text */
+  alertdone.classList.toggle("done");
+}
